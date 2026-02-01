96a052742acfd23dc787b13b79b1e5cd915f7572
boratashan <bora.tashan@gmail.com>
2026-02-01 22:31:34 +0100

    Tailwind support and Spartan

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { Home } from './home/home';
-
+import { HlmButtonImports } from '@spartan-ng/helm/button';
+import { HlmItemImports } from '@spartan-ng/helm/item';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, Home],
+  imports: [RouterOutlet, RouterLink, Home, HlmButtonImports, HlmItemImports], 
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
