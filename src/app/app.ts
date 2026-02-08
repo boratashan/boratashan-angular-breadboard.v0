b7686aaff7fc75c8831490e59f338bd199d36612
boratashan <bora.tashan@gmail.com>
2026-02-08 10:55:41 +0100

    Fixing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,22 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { Home } from './home/home';
-<<<<<<< HEAD
-import { HeaderComponent } from './header/header';
-import { UserFormComponent } from './user-form/user-form';
 
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, Home, HeaderComponent, UserFormComponent],
-=======
 import { HlmButtonImports } from '@spartan-ng/helm/button';
 import { HlmItemImports } from '@spartan-ng/helm/item';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, Home, HlmButtonImports, HlmItemImports], 
->>>>>>> 96a052742acfd23dc787b13b79b1e5cd915f7572
+  imports: [RouterOutlet, RouterLink, HlmButtonImports, HlmItemImports], 
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
